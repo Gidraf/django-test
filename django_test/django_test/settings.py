f723a5414be3c6bb64f91c2ee931b748bd342144
Gidraf <gidraforenja@outlook.com>
2021-01-04 21:31:33 +0300

    deployment

--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -24,7 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'dr109*ri$1z)oyp&_q#@qmgl7*1v6^gaa&&j=+y_9p$t=iiuf&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = []
 
